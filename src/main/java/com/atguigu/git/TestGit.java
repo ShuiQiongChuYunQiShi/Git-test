5a3ec63d51e1da05d682f16a298f1238bc05ddc3
水穷处云起时 <102282851+ShuiQiongChuYunQiShi@users.noreply.github.com>
2022-03-27 10:49:56 +0800

    delete form

--- src/main/java/com/atguigu/git/TestGit.java
+++ src/main/java/com/atguigu/git/TestGit.java
@@ -3,13 +3,6 @@ package com.atguigu.git;
 public class TestGit {
 
     public static void main(String[] args) {
-        System.out.println("haha");
-        System.out.println("hahaha");
-        System.out.println("hahahaha");
-        System.out.println("hot-fix");
-        System.out.println("master");
-        System.out.println("hot-fix 123");
-        System.out.println("pull test");
-        System.out.println("pull test");
+        int i = 0;
     }
 }
