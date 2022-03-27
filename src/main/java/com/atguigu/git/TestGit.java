a1100d696f74d9dc4a39777a8b2eef2f8d0654dd
水穷处云起时 <102282851+ShuiQiongChuYunQiShi@users.noreply.github.com>
2022-03-27 10:45:33 +0800

    pull commit

--- src/main/java/com/atguigu/git/TestGit.java
+++ src/main/java/com/atguigu/git/TestGit.java
@@ -9,5 +9,6 @@ public class TestGit {
         System.out.println("hot-fix");
         System.out.println("master");
         System.out.println("hot-fix 123");
+        System.out.println("pull test");
     }
 }
